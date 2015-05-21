958cb28dba608c26c0ef6e0b8e4ffe8c13cf190c
yni2 <yni2@ncsu.edu>
2015-05-21 12:13:45 -0400

    Added log

--- src/edu/ncsu/csc/Galant/algorithm/code/CodeIntegrator.java
+++ src/edu/ncsu/csc/Galant/algorithm/code/CodeIntegrator.java
@@ -40,11 +40,6 @@ import edu.ncsu.csc.Galant.GalantException;
 public class CodeIntegrator
 	/**
  	  * @todo Testing GitHub branch merging.
- 	  * @todo I notice that everytime algorithm is loaded and compile, newly-generated/recursive 
- 	  *		  code is being printed on console. I think that a good way for me to study the shortcommings 
- 	  *		  of the recursive mechnism is to examinate those code on console. Could you point out which 
- 	  *		  part of code triggers printing while the program is running?
- 	  * 
  	*/
 	{
 		public static final String METHOD_NAME = "runAlgorithm";
@@ -143,11 +138,10 @@ public class CodeIntegrator
 				String sourceCode = toJavaClass(className, userCode);
 				
                 // Display source code after macro processing
-                /*
                 LogHelper.setEnabled( true );
 				LogHelper.logDebug(sourceCode);
                 LogHelper.restoreState();
-				*/
+				
 				// Compile
 				DiagnosticCollector<JavaFileObject> diagnostics =
 					CompilerAndLoader.compile(qualifiedName, sourceCode);

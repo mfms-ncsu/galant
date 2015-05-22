15bc952a6f6dbe4a93450d51273708e0aace2fde
yni2 <yni2@ncsu.edu>
2015-05-21 20:33:08 -0400

    display line number

--- src/edu/ncsu/csc/Galant/algorithm/code/CodeIntegrator.java
+++ src/edu/ncsu/csc/Galant/algorithm/code/CodeIntegrator.java
@@ -54,21 +54,21 @@ public class CodeIntegrator
 		// compiled.
 		//@formatter:off
 		private static final String CLASS_STRUCTURE =
-			"package " + PACKAGE + ";" +
-			"import java.util.*;" +
-			"import edu.ncsu.csc.Galant.algorithm.Algorithm;" +
-			"import edu.ncsu.csc.Galant.graph.component.Graph;" +
-			"import edu.ncsu.csc.Galant.graph.component.Node;" +
-			"import edu.ncsu.csc.Galant.graph.component.Edge;" +
-			"import edu.ncsu.csc.Galant.algorithm.code.macro.Function;" +
-			"import edu.ncsu.csc.Galant.algorithm.code.macro.Pair;" +
-            "import edu.ncsu.csc.Galant.GalantException;" +
+			"package " + PACKAGE + ";\n" +
+			"import java.util.*;\n" +
+			"import edu.ncsu.csc.Galant.algorithm.Algorithm;\n" +
+			"import edu.ncsu.csc.Galant.graph.component.Graph;\n" +
+			"import edu.ncsu.csc.Galant.graph.component.Node;\n" +
+			"import edu.ncsu.csc.Galant.graph.component.Edge;\n" +
+			"import edu.ncsu.csc.Galant.algorithm.code.macro.Function;\n" +
+			"import edu.ncsu.csc.Galant.algorithm.code.macro.Pair;\n" +
+            "import edu.ncsu.csc.Galant.GalantException;\n" +
 			IMPORTS_FIELD +
 			"public class " + NAME_FIELD + " extends Algorithm" +
-				"{" +
+				"{\n" +
 					"@Override " +
 					"public void run() " +
-						String.format("{ try {%s}"
+						String.format("{ \n try { \n %s}"
                                       + " catch (Exception e)"
                                       + " { if ( e instanceof GalantException )"
                                       + " {GalantException ge = (GalantException) e;"

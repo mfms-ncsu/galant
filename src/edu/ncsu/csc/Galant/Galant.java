c03e1e40d0d4f7626cb3cb489aa875a9c88457a4
Matt Stallmann <mfms@ncsu.edu>
2016-08-09 21:00:02 -0400

    Added version information.

--- src/edu/ncsu/csc/Galant/Galant.java
+++ src/edu/ncsu/csc/Galant/Galant.java
@@ -13,7 +13,7 @@ import edu.ncsu.csc.Galant.gui.window.GraphWindow;
  */
 public class Galant {
 
-    public static final String VERSION = "v6.0";
+    public static final String VERSION = "v5.2.2";
 	public static void main(String[] args) {
 		ExceptionDialog.setDialogExceptionHandlerAsDefault();
 		SwingUtilities.invokeLater(new Runnable(){
@@ -34,4 +34,4 @@ public class Galant {
 	
 }
 
-//  [Last modified: 2016 06 28 at 14:01:19 GMT]
+//  [Last modified: 2016 08 10 at 00:38:09 GMT]

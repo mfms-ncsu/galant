bf5ebdec60944731b0603a8b4ca818721f18fe25
Matthias Stallmann <mfms@localhost.localdomain>
2017-01-05 14:55:51 -0500

    testing commit on VMware

--- src/edu/ncsu/csc/Galant/Galant.java
+++ src/edu/ncsu/csc/Galant/Galant.java
@@ -13,7 +13,7 @@ import edu.ncsu.csc.Galant.gui.window.GraphWindow;
  * @author Michael Owoc, Jason Cockrell, Alex McCabe, Ty Devries
  */
 public class Galant {
-  public static final String VERSION = "v5.4";
+  public static final String VERSION = "v6.0";
   public static void main(String[] args) {
     ExceptionDialog.setDialogExceptionHandlerAsDefault();
     SwingUtilities.invokeLater(new Runnable(){

8c5f6a1d1e05c99de0fb5783c80efe90d1a6e7f1
PriTanir <pritanir1@gmail.com>
2018-08-19 19:07:01 -0300

    using clone() to copy attributes in list
    Yet to copy each attribute individually without using attribute.clone()

--- src/edu/ncsu/csc/Galant/graph/component/AttributeList.java
+++ src/edu/ncsu/csc/Galant/graph/component/AttributeList.java
@@ -23,6 +23,12 @@ public class AttributeList implements Cloneable{
     public AttributeList copyAttributeList() throws CloneNotSupportedException
     {
         AttributeList clonedAttributeList=(AttributeList)super.clone();
+        ArrayList<Attribute> attributes=getAttributes();
+        for(Attribute attribute:attributes)
+        {
+            Attribute clonedAttribute=attribute.clone();
+            clonedAttributeList.add(attribute);
+        }
         return clonedAttributeList;
     }
     /**
